badb73c20339dbe27e3ac0bff9d1645b0f30dd92
Herlan <herlanherbas@gmail.com>
2023-08-13 16:36:28 -0400

    Se cambio el nombre de las variables en el componente Order

--- src/components/Orden/Order.jsx
+++ src/components/Orden/Order.jsx
@@ -2,25 +2,25 @@ import React, { useState } from "react";
 import { RiDeleteBin6Line } from "react-icons/ri";
 
 const Order = (props) => {
-  const { productos } = props;
+  const { order } = props;
   const { idMenu, setIdMenu } = props;
-  const {cambioFondo} = props
+  const {changeBackground} = props
 
   return (
-    <div className={`${cambioFondo ? "bg-slate-600":"bg-[#262837]"} block  p-4 rounded-xl mb-4`}>
+    <div className={`${changeBackground ? "bg-slate-600":"bg-[#262837]"} block  p-4 rounded-xl mb-4`}>
       <div className="grid grid-cols-5 mb-4 justify-between">
         {/* Product description */}
         <div className="col-span-4 flex items-center gap-3">
           <img
-            src={productos.imagen}
+            src={order.imagen}
             className="w-11 h-11 object-cover rounded-[50%]"
           />
           <div>
             <h5 className="text-[16px] text-orange-400">
-              {productos.nombreMenu}
+              {order.nombreMenu}
             </h5>
             <p className="text-[15px] text-white">
-              Cantidad: {productos.cantidad} platos
+              Cantidad: {order.cantidad} platos
             </p>
           </div>
         </div>
@@ -28,7 +28,7 @@ const Order = (props) => {
         {/* Price */}
         <div>
           <span className="text-[18px] text-white">
-            {productos.precioTotal}Bs
+            {order.precioTotal}Bs
           </span>
         </div>
       </div>
@@ -36,7 +36,7 @@ const Order = (props) => {
       <div className="flex justify-center">
         <div>
           <button
-            onClick={() => setIdMenu(productos.idMenu)}
+            onClick={() => setIdMenu(order.idMenu)}
             className="border border-red-600 p-2 rounded-lg"
           >
             <RiDeleteBin6Line className="text-red-600 font-bold" />
