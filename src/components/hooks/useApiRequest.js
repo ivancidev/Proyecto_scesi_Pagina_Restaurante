ea237761040b1744144ec3d4c9fe03df5a65b9fe
Herlan <herlanherbas@gmail.com>
2023-08-15 21:49:05 -0400

    Se optimizo el codigo del componente useApiRequest

--- src/components/hooks/useApiRequest.js
+++ src/components/hooks/useApiRequest.js
@@ -1,22 +1,23 @@
+import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 export const useApiRequest = (url, initialState = null) => {
   const [data, setData] = useState(initialState);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const responseData = await response.json();
-        setData(responseData);
-      } catch (err) {
-        setError(err);
-      }
-    };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+      setData(response.data);
+    } catch (error) {
+      setError(error);
+    }
+  };
 
+  useEffect(() => {
     fetchData();
   }, [url]);
 
   return { data, error };
 };
+
